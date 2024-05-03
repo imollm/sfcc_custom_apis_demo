83f113fa5a86828be3c0e91a2132ee2f5e2633a4
Ivan Moll <ivanmoll07@gmail.com>
2024-05-03 13:16:02 +0200

    Adapt other HTTP verbs

--- cartridges/app_helloworld_api/cartridge/rest-apis/hello-world/script.js
+++ cartridges/app_helloworld_api/cartridge/rest-apis/hello-world/script.js
@@ -10,4 +10,40 @@ exports.getHelloWorldMessage = function () {
     RESTResponseMgr.createSuccess(resObj).render();
 };
 
+exports.postHelloWorldMessage = function () {
+    var resObj = {
+        message: 'Hello World!'
+    };
+
+    RESTResponseMgr.createSuccess(resObj).render();
+};
+
+exports.putHelloWorldMessage = function () {
+    var resObj = {
+        message: 'Hello World!'
+    };
+
+    RESTResponseMgr.createSuccess(resObj).render();
+};
+
+exports.patchHelloWorldMessage = function () {
+    var resObj = {
+        message: 'Hello World!'
+    };
+
+    RESTResponseMgr.createSuccess(resObj).render();
+};
+
+exports.deleteHelloWorldMessage = function () {
+    var resObj = {
+        message: 'Hello World!'
+    };
+
+    RESTResponseMgr.createSuccess(resObj).render();
+};
+
 exports.getHelloWorldMessage.public = true;
+exports.postHelloWorldMessage.public = true;
+exports.putHelloWorldMessage.public = true;
+exports.patchHelloWorldMessage.public = true;
+exports.deleteHelloWorldMessage.public = true;
